210476deb64035957673faad4c9e588b07076370
GabeSal96 <gabesal96@gmail.com>
2021-07-07 10:06:10 -0700

    Implemented a feature where the player object looks at the mouse position on screen.

---
+++ Assets/Scripts/MouseLookAt.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MouseLookAt : MonoBehaviour
+{
+    [SerializeField]
+    private float _angleOffset = -90f;
+
+    private void Update()
+    {
+        float angle = ConvertMousePositionToLookAngle();
+
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    /// <summary>
+    /// Converts the mouse cursors position from the screen view into a world point minus the transforms current position.
+    /// </summary>
+    /// <returns>angle is the float value that represents the angle between the mouse and gameobject</returns>
+    private float ConvertMousePositionToLookAngle()
+    {
+        Vector3 playerPositionOnScreen = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 direction = Input.mousePosition - playerPositionOnScreen;
+        
+        float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) + _angleOffset;
+
+        return angle;
+    }
+}
