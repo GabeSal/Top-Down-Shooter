b1f3ca0b34998fe4f0ace19c21f82207baa468b7
GabeSal96 <gabesal96@gmail.com>
2021-07-21 11:48:37 -0700

    Added crosshair image to replace mouse cursor.

--- Assets/Scripts/UI/CursorBehaviour.cs
+++ Assets/Scripts/UI/CursorBehaviour.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CursorBehaviour : MonoBehaviour
 {
+    public Texture2D crosshair;
+
     private KeyCode _escapeKey = KeyCode.Escape;
     private int _leftMouseClick = 0;
 
     private void Awake()
     {
+        Vector2 centerPivot = new Vector2(crosshair.width / 2, crosshair.height / 2);
+
         Cursor.lockState = CursorLockMode.Confined;
+        Cursor.SetCursor(crosshair, centerPivot, CursorMode.Auto);
     }
 
     private void Update()
